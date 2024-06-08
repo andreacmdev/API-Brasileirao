ae1a0dc57c08c6c0a206183d9202b78745b14109
Andre Machado <mmachadoacm@gmail.com>
2024-06-07 23:30:48 -0300

    atualizando campos do objeto

--- nodejs-brasileirao/app.js
+++ nodejs-brasileirao/app.js
@@ -3,6 +3,8 @@ import express from 'express'
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (requisicao, resposta) => {
     resposta.status(200).send(tabela2024);
 });
@@ -10,11 +12,25 @@ app.get('/', (requisicao, resposta) => {
 app.get('/:sigla', (requisicao, resposta) => {
     const siglaInformada = requisicao.params.sigla.toUpperCase();
     const time = tabela2024.find(infoTime => infoTime.sigla === siglaInformada);
-    if (!time) { //undefined se comportal como falso sempre que definimos ele como boolean
-        resposta.status(404).send('Não existe na série A do brasileirão um time com a sigla informada');
+    if (!time) { //undefined se comporta como falso sempre que definimos ele como boolean
+        resposta.status(404).send(
+            'Não existe na série A do brasileirão um time com a sigla informada'
+        );
         return;
     }
     resposta.status(200).send(time);
 });
+
+app.put('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase();
+    const timeSelecionado = tabela2024.find((t) => t.sigla === siglaInformada);
+    console.log(req.body);
+    const campos = Object.keys(req.body);
+    for (let campo of campos) {
+       timeSelecionado[campo] = req.body[campo]
+    }
+    res.status(200).send(timeSelecionado);
+
+})
 
 app.listen(300, () => console.log('servidor rodando com sucesso'));
